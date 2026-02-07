6a19a93c7d6e465d6d2b3e670249db68cc54cc91
imad ASSERNE <imad.asserne@gmail.com>
2026-02-07 23:46:58 +0100

    feat : add routing to app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path:'login', 
+        loadComponent: () => import('./features/account/login/login.component')
+        .then(m => m.LoginComponent)
+    },  
+    {path:'register', 
+        loadComponent: () => import('./features/account/register/register.component')
+        .then(m => m.RegisterComponent)
+    },
+    {path:'board', 
+        loadComponent: () => import('./features/boards/list/list.component')
+        .then(m => m.ListComponent)
+    },
+    {path:'detail/:id', 
+        loadComponent: () => import('./features/boards/detail/detail.component')
+        .then(m => m.DetailComponent)
+    },
+];

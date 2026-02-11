cff7f9f9128aa6e074e659672b839ebd49a7a816
imad ASSERNE <imad.asserne@gmail.com>
2026-02-11 23:12:22 +0100

    remove : @OnDelete annotation

--- Task-Manager-BackEnd/src/main/java/com/IMADWRGH/task_manager/entities/User.java
+++ Task-Manager-BackEnd/src/main/java/com/IMADWRGH/task_manager/entities/User.java
@@ -1,14 +1,11 @@
 package com.IMADWRGH.task_manager.entities;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "app_user")   // 'user' is a reserved keyword in many databases
+@Table(name = "app_user")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -34,17 +31,14 @@ public class User {
     @Column(nullable = false)
     private Boolean emailVerified = false;
 
-    // Owner side of ManyToMany with Board
     @ManyToMany
     @JoinTable(
             name = "user_board",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "board_id")
     )
-    @OnDelete(action = OnDeleteAction.CASCADE)   // delete join row when user/board is deleted
     private List<Board> boards = new ArrayList<>();
 
-    // One User can be assigned to many Cards
     @OneToMany(mappedBy = "assignee")
     private List<Card> cards = new ArrayList<>();
 }

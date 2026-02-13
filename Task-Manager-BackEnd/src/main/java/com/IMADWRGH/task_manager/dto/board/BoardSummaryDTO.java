6164b95b03a81db1afb95b26b82ae1e6320d4b23
imad ASSERNE <imad.asserne@gmail.com>
2026-02-13 22:13:58 +0100

    feat: add BoardSummaryDTO

---
+++ Task-Manager-BackEnd/src/main/java/com/IMADWRGH/task_manager/dto/board/BoardSummaryDTO.java
@@ -0,0 +1,14 @@
+package com.IMADWRGH.task_manager.dto.board;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardSummaryDTO {
+
+    private Long id;
+    private String name;
+}

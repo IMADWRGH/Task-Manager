721a648a32b11d05b048dae6ea8e244b49b58a32
imad ASSERNE <imad.asserne@gmail.com>
2026-02-13 22:18:11 +0100

    feat: add SwimlaneSummaryDTO

---
+++ Task-Manager-BackEnd/src/main/java/com/IMADWRGH/task_manager/dto/swimlane/SwimlaneSummaryDTO.java
@@ -0,0 +1,15 @@
+package com.IMADWRGH.task_manager.dto.swimlane;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SwimlaneSummaryDTO {
+
+    private Long id;
+    private String name;
+    private Integer order;
+}

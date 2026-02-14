6792e0910e017b8580d38e3df371e16928181d78
imad ASSERNE <imad.asserne@gmail.com>
2026-02-15 00:09:49 +0100

    feat : Implementing UserDetails interface

--- Task-Manager-BackEnd/src/main/java/com/IMADWRGH/task_manager/entities/User.java
+++ Task-Manager-BackEnd/src/main/java/com/IMADWRGH/task_manager/entities/User.java
@@ -1,7 +1,12 @@
 package com.IMADWRGH.task_manager.entities;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -10,7 +15,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -45,4 +50,34 @@ public class User {
 
     @OneToMany(mappedBy = "assignee")
     private List<Card> cards = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("Role "+role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return emailVerified;
+    }
 }

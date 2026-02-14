443e6c81788ee099c2476590bb1350cb4a93b442
imad ASSERNE <imad.asserne@gmail.com>
2026-02-14 21:24:06 +0100

    feat: add UserSummaryDTO

---
+++ Task-Manager-BackEnd/src/main/java/com/IMADWRGH/task_manager/dto/user/UserSummaryDTO.java
@@ -0,0 +1,14 @@
+package com.IMADWRGH.task_manager.dto.user;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSummaryDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}

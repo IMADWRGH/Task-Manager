df7363210846624336336e10b770431b7e56e67b
imad ASSERNE <imad.asserne@gmail.com>
2026-08-25 23:10:56 +0200

    refactor: remove unused repository interfaces for Board, Card, Swimlane, and User entities

--- Task-Manager-BackEnd/src/main/java/com/IMADWRGH/task_manager/repository/BoardRepo.java
+++
@@ -1,9 +0,0 @@
-package com.IMADWRGH.task_manager.repository;
-
-import com.IMADWRGH.task_manager.entities.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepo extends JpaRepository<Board,Long> {
-}
